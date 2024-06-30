875791d7f90da1abdf22e57a32805f62cfa29519
Adriana Reis <95719825+AdrianaRCosta@users.noreply.github.com>
2024-06-30 15:24:17 -0300

    Add files via upload

---
+++ Player.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+
+# Declara a variável do tipo AnimationPlayer e a instancia com o nó AnimationPlayer
+ # var animation_player: AnimationPlayer = $AnimationPlayer
+
+var speed: float = 3
+
+var is_running: bool = false
+ 
+ #func _process(delta: float) -> void:
+#	if Input.is_action_just_pressed("ui_accept"):
+	#	if is_running:
+		#	animation_player.play("Idle")
+		#	is_running = false
+	#	else:
+		#	animation_player.play("rum")
+	#		is_running = true
+
+
+	
+
+
+
